f6191777f1f3404a3c9951c1a4ecd76c58e7ae41
Bin ZHOU <zhoubinxyz@gmail.com>
2018-07-05 09:38:05 -0400

    .

--- helpers.py
+++
@@ -1,7 +0,0 @@
-"""Helpers for PyTorch-ES examples"""
-
-
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Linear') != -1:
-        m.weight.data.normal_(0.0, 0.02)
